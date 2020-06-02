ba26052bcce9efc191908add1f9c6404717205ea
MMcClure11 <mcclme02@luther.edu>
2020-06-02 17:30:20 -0500

    creates migration and model for users

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :username, uniqueness: true
+  validates :username, presence: true
+
+  validates :email, uniqueness: true
+  validates :email, presence: true
+
+  has_many :recipes
+end
