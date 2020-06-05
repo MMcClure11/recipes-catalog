2030f46d91c7165f267680c5543137fa6196388f
MMcClure11 <mcclme02@luther.edu>
2020-06-05 18:22:28 -0500

    adds show page for single recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,10 @@ class RecipesController < ApplicationController
     erb :'/recipes/index'
   end
 
+  get '/recipes/:slug' do
+    authenticate
+    @recipe = Recipe.find_by_slug(params[:slug])
+    erb :'recipes/show'
+  end
+
   end
