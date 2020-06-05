625d160d71a23adb51b37ae429f5caa8e1b373eb
MMcClure11 <mcclme02@luther.edu>
2020-06-05 15:54:12 -0500

    adds helpers methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,25 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  helpers do
+
+    def current_user
+      User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+      !!current_user
+    end
+
+    def authenticate
+      redirect '/login' if !logged_in?
+    end
+
+    def authorize(resource)
+      authenticate
+      redirect '/recipes' if resource.user != current_user
+    end
+
+  end
+
 end
