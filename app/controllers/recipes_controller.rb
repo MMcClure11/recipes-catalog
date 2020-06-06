c2e2fbd8f11262ab59339a80d1a928035ee1ed67
MMcClure11 <mcclme02@luther.edu>
2020-06-05 20:55:42 -0500

    adds routes and form for a user to make a new recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,16 +6,55 @@ class RecipesController < ApplicationController
     erb :'/recipes/index'
   end
 
-  get '/recipes/:slug' do
+  get '/recipes/new' do
     authenticate
+    erb :'/recipes/new'
+  end
+
+  get '/recipes/:slug' do
     @recipe = Recipe.find_by_slug(params[:slug])
+    authorize(@recipe)
     erb :'recipes/show'
   end
 
+  post '/recipes' do
+    authenticate
+    @recipe = Recipe.create(name: params[:name], serving_size: params[:serving_size], cook_time: params[:cook_time], ingredients: params[:ingredients], instructions: params[:instructions], user: current_user)
+    redirect "/recipes/#{@recipe.slug}"
+  end
+
+  get '/recipes/:slug/edit' do 
+
+  end
+
+  patch '/recipes/:slug' do 
+
+  end
+
   delete '/recipes/:slug' do
-    @recipe = Recipe.find_by_slug(params[:slug])
-    authorize(@recipe)
-    @recipe.destroy
+    recipe = Recipe.find_by_slug(params[:slug])
+    authorize(recipe)
+    recipe.destroy
     redirect '/recipes'
   end
 end
+
+# for recipes/show
+# <h3>Categories</h3>
+# <ul>
+#   <% @recipe.categories.each do |category| %>
+#     <li><%= category.name %></li>
+#   <% end %>
+# </ul>
+
+#for recipes/new
+# <label for="category">Choose a Category:</label><br>
+# <% Category.all.each do |category| %>
+#   <input type="checkbox" name="category[name][]" id="category_<%= category.id %>" value="<%= category.id %>"><%= category.name %></input><br>
+# <% end %>
+# <br>
+# <p>Or Create a Category:</p>
+#   <label for="new_category">Category Name:</label>
+#   <input type="text" name="category[name]">
+# <br><br>
+# 
