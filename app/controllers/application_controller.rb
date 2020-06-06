3b2fbc09a32ad590bead7a0417a9dbf2b3ea20d7
MMcClure11 <mcclme02@luther.edu>
2020-06-06 14:31:44 -0500

    changes session_secret to environment variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < Sinatra::Base
   configure do
     set :views, -> {File.join(root, "../views")}
     enable :sessions
-    set :session_secret, "password_security"
+    set :session_secret, ENV['SESSION_SECRET']
   end
 
   get '/' do
