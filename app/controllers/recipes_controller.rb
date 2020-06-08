8185d84ad8922b58c6a1d91cdf6fc68f2148095c
MMcClure11 <mcclme02@luther.edu>
2020-06-08 14:07:01 -0500

    sorts categories alphabetically in new form, trying to requuire category functionality

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -19,10 +19,10 @@ class RecipesController < ApplicationController
 
   post '/recipes' do
     authenticate
-    if !params[:categories] && params[:category][:name].empty?
-      @error = "Please select a category or create a new category."
-      erb :'/recipes/new'
-    else
+    # if !params[:categories] && params[:category][:name].empty?
+    #   @error = "Please select a category or create a new category."
+    #   erb :'/recipes/new'
+    # else
       @recipe = Recipe.create(name: params[:name], 
         serving_size: params[:serving_size], 
         cook_time: params[:cook_time], 
@@ -35,7 +35,7 @@ class RecipesController < ApplicationController
       end
       @recipe.save
       redirect "/recipes/#{@recipe.id}"
-    end
+    # end
   end
 
   get '/recipes/:id/edit' do 
@@ -45,22 +45,27 @@ class RecipesController < ApplicationController
   end
 
   patch '/recipes/:id' do 
+    @recipe = Recipe.find_by(id: params[:id])
+    authorize(@recipe)
     if !params[:recipe].keys.include?("category_ids")
       params[:recipe]["category_ids"] = []
     end
-    @recipe = Recipe.find_by(id: params[:id])
-    authorize(@recipe)
-    @recipe.update(name: params[:recipe][:name], 
-      serving_size: params[:serving_size], 
-      cook_time: params[:cook_time], 
-      ingredients: params[:ingredients], 
-      instructions: params[:instructions])
-      @recipe.category_ids = params[:recipe][:category_ids]
-    if !params[:category][:name].empty?
-      @recipe.categories << Category.create(params[:category])
-    end
-    @recipe.save
-    redirect "/recipes/#{@recipe.id}"
+    # if !params[:categories] && params[:category][:name].empty?
+    #   @error = "Please select a category or create a new category."
+    #   erb :"/recipes/edit"
+    # else
+      @recipe.update(name: params[:recipe][:name], 
+        serving_size: params[:serving_size], 
+        cook_time: params[:cook_time], 
+        ingredients: params[:ingredients], 
+        instructions: params[:instructions])
+        @recipe.category_ids = params[:recipe][:category_ids]
+      if !params[:category][:name].empty?
+        @recipe.categories << Category.create(params[:category])
+      end
+      @recipe.save
+      redirect "/recipes/#{@recipe.id}"
+    # end
   end
 
   delete '/recipes/:id' do
