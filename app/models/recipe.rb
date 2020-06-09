50b13677899904d57586c005c7927628d1f695db
MMcClure11 <mcclme02@luther.edu>
2020-06-09 09:52:20 -0500

    requires secrets.rb in env.rb, adds has_many relationship for recipe_ingredients to recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,4 +7,7 @@ class Recipe < ActiveRecord::Base
   belongs_to :user
   has_many :recipe_categories
   has_many :categories, through: :recipe_categories
+
+  # has_many :recipe_ingredients
+  # has_many :ingredients, through: :recipe_ingredients
 end
