8125a257277ea7301c3036771fc580fc23dc1de8
MMcClure11 <mcclme02@luther.edu>
2020-06-11 10:27:32 -0500

    adds user recipes to category show page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,4 +6,11 @@ class CategoriesController < ApplicationController
     erb :'/categories/index'
   end
 
+  get '/categories/:id' do
+    authenticate
+    @user = current_user
+    @category = Category.find_by(id: params[:id])
+    erb :'/categories/show'
+  end
+
 end
