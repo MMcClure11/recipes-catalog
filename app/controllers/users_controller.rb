8185222723336303645436b3bbcb33f63ea494a6
MMcClure11 <mcclme02@luther.edu>
2020-06-12 17:12:00 -0500

    changes slug to id to prevent user conflicts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,14 @@ class UsersController < ApplicationController
     erb :'/users/dashboard'
   end
 
-  get '/users/:slug/edit' do
-    @user = User.find_by_slug(params[:slug])
+  get '/users/:id/edit' do
+    @user = User.find_by(id: params[:id])
     authorize_user(@user)
     erb :'/users/edit'
   end
 
-  patch '/users/:slug' do
-    @user = User.find_by_slug(params[:slug])
+  patch '/users/:id' do
+    @user = User.find_by(id: params[:id])
     authorize_user(@user)
     @user.update(name: params[:name].gsub(/[\<\>\/]/, ""), 
       email: params[:email].gsub(/[\<\>\/]/, ""),
