182349ca1398c06ec30e7bf70cddae8a38edc73b
MMcClure11 <mcclme02@luther.edu>
2020-06-13 14:47:25 -0500

    sanitizes recipe category, adds errors in edit form, prevents users from entering in empty category name

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -69,11 +69,19 @@ class RecipesController < ApplicationController
     @recipe.create_recipe_ingredient_from(params[:recipe_ingredients])
 
     @recipe.category_ids = params[:recipe][:category_ids]
-    if !params[:category][:name].empty? && !@recipe.categories.include?(Category.find_by(name: params[:category][:name].downcase.capitalize))
-      @recipe.categories << Category.find_or_create_by(name: params[:category][:name].downcase.capitalize)
+    if !params[:category][:name].empty? && !@recipe.categories.include?(Category.find_by(name: sanitize(params[:category][:name]).downcase.capitalize))
+      @category = Category.find_or_create_by(name: sanitize(params[:category][:name]).downcase.capitalize)
+      if @category.persisted?
+        @recipe.categories << @category
+      else
+        return(erb :'/recipes/edit') 
+      end
     end
-    @recipe.save
-    redirect "/recipes/#{@recipe.id}"
+    if @recipe.save
+      redirect "/recipes/#{@recipe.id}"
+    else
+      erb :'/recipes/edit'
+    end
   end
 
   delete '/recipes/:id' do
