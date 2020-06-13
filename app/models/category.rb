178de3d323be468436151768d677d2a990274b3e
MMcClure11 <mcclme02@luther.edu>
2020-06-13 12:05:52 -0500

    adds validation and logic for category name

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,11 @@ class Category < ActiveRecord::Base
   has_many :recipe_categories
   has_many :recipes, through: :recipe_categories
 
+  before_validation :trim_whitespace
+  validates :name, presence: true 
+
+  def trim_whitespace
+    name&.strip
+  end
+
 end
